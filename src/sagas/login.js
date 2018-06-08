0787603a7a9530c0b5603e977b9bfaa8d1dd4dd9
vsmirnova <vsmirnova@mmtr.ru>
2018-06-08 11:55:00 +0300

    saga

--- src/sagas/login.js
+++ src/sagas/login.js
@@ -1,11 +1,23 @@
 import {call, put, takeEvery} from 'redux-saga/effects';
 import * as appActions from '../actions';
 import * as actionTypes from '../actions/actionsTypes.js';
+import axios from "axios";
 
 export function* authorization(action) {
     try {
         console.log("authorization", action);
-        const {values, func} = action.payload;
+        const {values, func} = action.payload,
+            send = {
+                "header": {
+                    "Content-Type": "application/json"
+                },
+                "method": "POST",
+                "url": "http://smironovich.diasoft.ru:8090/login",
+                "data": values
+            },
+            res = yield call(axios, send);
+
+        console.log("res", res);
 
         if (values.login === values.password) {
             func();
